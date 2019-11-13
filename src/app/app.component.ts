02e761ed8fe1bf928f4262421a297cf37e1dde8c
Romero <romero.melo@facepe.br>
2019-11-13 02:38:52 -0300

    Inicialização do projeto, configurações iniciais de ambiemte e diagramação do layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend-transportadora';
+
+  tiles: Tile[] = [
+    {text: '', cols: 1, rows: 4, color: 'lightblue'},
+    {text: '', cols: 3, rows: 2, color: 'lightgreen'},
+    {text: '', cols: 4, rows: 1, color: 'softorange'},
+  ];
+
+  ngOnInit(){
+  }
 }
